6a3e79fbcd2304ae693802d51586c356bf55ae1c
elinuxhenrik <henrik.b.andersson@est.tech>
2018-08-17 12:34:58 +0200

    Add seed code.
    First version based on PRH micro service.
    
    Change-Id: Iea1673a8a1961006b1ea98ef245e213e3652eb82
    Issue-ID: DCAEGEN2-638
    Signed-off-by: elinuxhenrik <henrik.b.andersson@est.tech>

---
+++ datafile-app-server/src/test/java/org/onap/dcaegen2/collectors/datafile/tasks/DmaapPublisherTaskImplTest.java
@@ -0,0 +1,124 @@
+/*
+ * ============LICENSE_START=======================================================
+ * PROJECT
+ * ================================================================================
+ * Copyright (C) 2018 NOKIA Intellectual Property. All rights reserved.
+ * ================================================================================
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * ============LICENSE_END=========================================================
+ */
+
+package org.onap.dcaegen2.collectors.datafile.tasks;
+
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.doReturn;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.spy;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.verifyNoMoreInteractions;
+import static org.mockito.Mockito.when;
+
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.function.Executable;
+import org.onap.dcaegen2.collectors.datafile.config.DmaapPublisherConfiguration;
+import org.onap.dcaegen2.collectors.datafile.configuration.AppConfig;
+import org.onap.dcaegen2.collectors.datafile.exceptions.DmaapNotFoundException;
+import org.onap.dcaegen2.collectors.datafile.exceptions.DatafileTaskException;
+import org.onap.dcaegen2.collectors.datafile.model.ConsumerDmaapModel;
+import org.onap.dcaegen2.collectors.datafile.service.producer.DMaaPProducerReactiveHttpClient;
+import org.onap.dcaegen2.collectors.datafile.tasks.DmaapPublisherTaskImpl;
+import org.onap.dcaegen2.collectors.datafile.config.ImmutableDmaapPublisherConfiguration;
+import org.onap.dcaegen2.collectors.datafile.model.ImmutableConsumerDmaapModel;
+import org.springframework.http.HttpStatus;
+import reactor.core.publisher.Mono;
+import reactor.test.StepVerifier;
+
+/**
+ * @author <a href="mailto:przemyslaw.wasala@nokia.com">Przemysław Wąsala</a> on 5/17/18
+ */
+class DmaapPublisherTaskImplTest {
+
+    private static ConsumerDmaapModel consumerDmaapModel;
+    private static DmaapPublisherTaskImpl dmaapPublisherTask;
+    private static DMaaPProducerReactiveHttpClient dMaaPProducerReactiveHttpClient;
+    private static AppConfig appConfig;
+    private static DmaapPublisherConfiguration dmaapPublisherConfiguration;
+
+    @BeforeAll
+    static void setUp() {
+        dmaapPublisherConfiguration = new ImmutableDmaapPublisherConfiguration.Builder()
+            .dmaapContentType("application/json").dmaapHostName("54.45.33.2").dmaapPortNumber(1234)
+            .dmaapProtocol("https").dmaapUserName("Datafile").dmaapUserPassword("Datafile")
+            .dmaapTopicName("unauthenticated.SEC_OTHER_OUTPUT").build();
+        consumerDmaapModel = ImmutableConsumerDmaapModel.builder().ipv4("10.16.123.234")
+            .ipv6("0:0:0:0:0:FFFF:0A10:7BEA")
+            .pnfName("NOKQTFCOC540002E").build();
+        appConfig = mock(AppConfig.class);
+    }
+
+    @Test
+    void whenPassedObjectDoesntFit_ThrowsDatafileTaskException() {
+        //given
+        when(appConfig.getDmaapPublisherConfiguration()).thenReturn(dmaapPublisherConfiguration);
+        dmaapPublisherTask = new DmaapPublisherTaskImpl(appConfig);
+
+        //when
+        Executable executableFunction = () -> dmaapPublisherTask.execute(null);
+
+        //then
+        assertThrows(DatafileTaskException.class, executableFunction, "The specified parameter is incorrect");
+    }
+
+    @Test
+    void whenPassedObjectFits_ReturnsCorrectStatus() throws DatafileTaskException {
+        //given
+        prepareMocksForTests(HttpStatus.OK.value());
+
+        //when
+        StepVerifier.create(dmaapPublisherTask.execute(Mono.just(consumerDmaapModel))).expectSubscription()
+            .expectNext(HttpStatus.OK.toString()).verifyComplete();
+
+        //then
+        verify(dMaaPProducerReactiveHttpClient, times(1))
+            .getDMaaPProducerResponse(any(Mono.class));
+        verifyNoMoreInteractions(dMaaPProducerReactiveHttpClient);
+    }
+
+
+    @Test
+    void whenPassedObjectFits_butIncorrectResponseReturns() throws DmaapNotFoundException {
+        //given
+        prepareMocksForTests(HttpStatus.UNAUTHORIZED.value());
+
+        //when
+        StepVerifier.create(dmaapPublisherTask.execute(Mono.just(consumerDmaapModel))).expectSubscription()
+            .expectNext(String.valueOf(HttpStatus.UNAUTHORIZED.value())).verifyComplete();
+
+        //then
+        verify(dMaaPProducerReactiveHttpClient, times(1)).getDMaaPProducerResponse(any(Mono.class));
+        verifyNoMoreInteractions(dMaaPProducerReactiveHttpClient);
+    }
+
+
+    private void prepareMocksForTests(Integer httpResponseCode) {
+        dMaaPProducerReactiveHttpClient = mock(DMaaPProducerReactiveHttpClient.class);
+        when(dMaaPProducerReactiveHttpClient.getDMaaPProducerResponse(any(Mono.class)))
+            .thenReturn(Mono.just(httpResponseCode.toString()));
+        dmaapPublisherTask = spy(new DmaapPublisherTaskImpl(appConfig));
+        when(dmaapPublisherTask.resolveConfiguration()).thenReturn(dmaapPublisherConfiguration);
+        doReturn(dMaaPProducerReactiveHttpClient).when(dmaapPublisherTask).resolveClient();
+    }
+}
---
+++ datafile-dmaap-client/src/test/java/org/onap/dcaegen2/collectors/datafile/service/consumer/DMaaPConsumerReactiveHttpClientTest.java
@@ -0,0 +1,122 @@
+/*
+ * ============LICENSE_START=======================================================
+ * PNF-REGISTRATION-HANDLER
+ * ================================================================================
+ * Copyright (C) 2018 NOKIA Intellectual Property. All rights reserved.
+ * ================================================================================
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * ============LICENSE_END=========================================================
+ */
+
+package org.onap.dcaegen2.collectors.datafile.service.consumer;
+
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.doReturn;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.spy;
+import static org.mockito.Mockito.when;
+import static org.springframework.web.reactive.function.client.ExchangeFilterFunctions.basicAuthentication;
+
+import java.net.URI;
+import java.net.URISyntaxException;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.onap.dcaegen2.collectors.datafile.config.DmaapConsumerConfiguration;
+import org.onap.dcaegen2.collectors.datafile.service.consumer.DMaaPConsumerReactiveHttpClient;
+import org.springframework.http.HttpHeaders;
+import org.springframework.web.reactive.function.client.WebClient;
+import org.springframework.web.reactive.function.client.WebClient.RequestHeadersUriSpec;
+import org.springframework.web.reactive.function.client.WebClient.ResponseSpec;
+import reactor.core.publisher.Mono;
+import reactor.test.StepVerifier;
+
+/**
+ * @author <a href="mailto:przemyslaw.wasala@nokia.com">Przemysław Wąsala</a> on 6/27/18
+ */
+class DMaaPConsumerReactiveHttpClientTest {
+
+    private DMaaPConsumerReactiveHttpClient dmaapConsumerReactiveHttpClient;
+
+    private DmaapConsumerConfiguration consumerConfigurationMock = mock(DmaapConsumerConfiguration.class);
+    private static final String JSON_MESSAGE = "{ \"responseFromDmaap\": \"Success\"}";
+    private Mono<String> expectedResult = Mono.empty();
+    private WebClient webClient;
+    private RequestHeadersUriSpec requestHeadersSpec;
+    private ResponseSpec responseSpec;
+
+
+    @BeforeEach
+    void setUp() {
+        when(consumerConfigurationMock.dmaapHostName()).thenReturn("54.45.33.2");
+        when(consumerConfigurationMock.dmaapProtocol()).thenReturn("https");
+        when(consumerConfigurationMock.dmaapPortNumber()).thenReturn(1234);
+        when(consumerConfigurationMock.dmaapUserName()).thenReturn("Datafile");
+        when(consumerConfigurationMock.dmaapUserPassword()).thenReturn("Datafile");
+        when(consumerConfigurationMock.dmaapContentType()).thenReturn("application/json");
+        when(consumerConfigurationMock.dmaapTopicName()).thenReturn("unauthenticated.SEC_OTHER_OUTPUT");
+        when(consumerConfigurationMock.consumerGroup()).thenReturn("OpenDCAE-c12");
+        when(consumerConfigurationMock.consumerId()).thenReturn("c12");
+
+        dmaapConsumerReactiveHttpClient = new DMaaPConsumerReactiveHttpClient(consumerConfigurationMock);
+        webClient = spy(WebClient.builder()
+            .defaultHeader(HttpHeaders.CONTENT_TYPE, consumerConfigurationMock.dmaapContentType())
+            .filter(basicAuthentication(consumerConfigurationMock.dmaapUserName(),
+                consumerConfigurationMock.dmaapUserPassword()))
+            .build());
+        requestHeadersSpec = mock(RequestHeadersUriSpec.class);
+        responseSpec = mock(ResponseSpec.class);
+    }
+
+
+    @Test
+    void getHttpResponse_Success() {
+        //given
+        expectedResult = Mono.just(JSON_MESSAGE);
+
+        //when
+        mockDependantObjects();
+        doReturn(expectedResult).when(responseSpec).bodyToMono(String.class);
+        dmaapConsumerReactiveHttpClient.createDMaaPWebClient(webClient);
+        Mono<String> response = dmaapConsumerReactiveHttpClient.getDMaaPConsumerResponse();
+
+        //then
+        StepVerifier.create(response).expectSubscription()
+            .expectNextMatches(results -> {
+                Assertions.assertEquals(results, expectedResult.block());
+                return true;
+            }).verifyComplete();
+    }
+
+    @Test
+    void getHttpResponse_whenUriSyntaxExceptionHasBeenThrown() throws URISyntaxException {
+        //given
+        dmaapConsumerReactiveHttpClient = spy(dmaapConsumerReactiveHttpClient);
+        //when
+        when(webClient.get()).thenReturn(requestHeadersSpec);
+        dmaapConsumerReactiveHttpClient.createDMaaPWebClient(webClient);
+        when(dmaapConsumerReactiveHttpClient.getUri()).thenThrow(URISyntaxException.class);
+
+        //then
+        StepVerifier.create(dmaapConsumerReactiveHttpClient.getDMaaPConsumerResponse()).expectSubscription()
+            .expectError(Exception.class).verify();
+    }
+
+    private void mockDependantObjects() {
+        when(webClient.get()).thenReturn(requestHeadersSpec);
+        when(requestHeadersSpec.uri((URI) any())).thenReturn(requestHeadersSpec);
+        when(requestHeadersSpec.retrieve()).thenReturn(responseSpec);
+        doReturn(responseSpec).when(responseSpec).onStatus(any(), any());
+    }
+
+}
