04ecd8145b97c37b641e77eee24ed2fe2e44ccef
micdzied <michal.1.dziedzic@nokia.com>
2018-10-23 14:58:43 +0200

    add test
    Change-Id: I696421423fe0b6d9634bb2ef90829b53ac64a7b0
    Issue-ID: DCAEGEN2-889
    Signed-off-by: micdzied <michal.1.dziedzic@nokia.com>

---
+++ datafile-dmaap-client/src/test/java/org/onap/dcaegen2/collectors/datafile/service/HttpUtilsTest.java
@@ -0,0 +1,35 @@
+/*
+ * ============LICENSE_START======================================================================
+ * Copyright (C) 2018 NOKIA Intellectual Property, 2018 Nordix Foundation. All rights reserved.
+ * ===============================================================================================
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except
+ * in compliance with the License. You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed under the License
+ * is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express
+ * or implied. See the License for the specific language governing permissions and limitations under
+ * the License.
+ * ============LICENSE_END========================================================================
+ */
+
+package org.onap.dcaegen2.collectors.datafile.service;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+class HttpUtilsTest {
+
+    @Test
+    public void shouldReturnSuccessfulResponse() {
+        assertTrue(HttpUtils.isSuccessfulResponseCode(200));
+    }
+
+    @Test
+    public void shouldReturnBadResponse() {
+        assertFalse(HttpUtils.isSuccessfulResponseCode(404));
+    }
+}
