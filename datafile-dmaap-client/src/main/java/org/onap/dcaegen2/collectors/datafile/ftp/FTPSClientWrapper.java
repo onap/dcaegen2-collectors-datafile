67dd4223056a2c6a56ffef34076db7dc1a2740b0
Chengkai Yan <martin.c.yan@est.tech>
2018-11-23 16:23:56 +0100

    fix bugs in jira DCAEGEN2-940 and DCAEGEN2-941
    Change-Id: Id0b3e295cab0e085746b034caccbf82aca2e0d7b
    Signed-off-by: Chengkai Yan <martin.c.yan@est.tech>
    Issue-ID: DCAEGEN2-940
    Issue-ID: DCAEGEN2-941

--- datafile-dmaap-client/src/main/java/org/onap/dcaegen2/collectors/datafile/ftp/FTPSClientWrapper.java
+++ datafile-dmaap-client/src/main/java/org/onap/dcaegen2/collectors/datafile/ftp/FTPSClientWrapper.java
@@ -2,17 +2,15 @@
  * ============LICENSE_START======================================================================
  * Copyright (C) 2018 Nordix Foundation. All rights reserved.
  * ===============================================================================================
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except
+ * in compliance with the License. You may obtain a copy of the License at
  *
- *      http://www.apache.org/licenses/LICENSE-2.0
+ * http://www.apache.org/licenses/LICENSE-2.0
  *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
+ * Unless required by applicable law or agreed to in writing, software distributed under the License
+ * is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express
+ * or implied. See the License for the specific language governing permissions and limitations under
+ * the License.
  * ============LICENSE_END========================================================================
  */
 
@@ -75,6 +73,11 @@ public class FTPSClientWrapper implements IFTPSClient {
     }
 
     @Override
+    public void setFileType(int fileType) throws IOException {
+        ftpsClient.setFileType(fileType);
+    }
+
+    @Override
     public void execPBSZ(int psbz) throws IOException {
         ftpsClient.execPBSZ(psbz);
     }
@@ -92,5 +95,15 @@ public class FTPSClientWrapper implements IFTPSClient {
     @Override
     public void setTimeout(Integer t) {
         this.ftpsClient.setDefaultTimeout(t);
+    }
+
+    @Override
+    public boolean isConnected() {
+        return ftpsClient.isConnected();
+    }
+
+    @Override
+    public void setBufferSize(int bufSize) {
+        ftpsClient.setBufferSize(bufSize);
     }
 }
--- datafile-dmaap-client/src/main/java/org/onap/dcaegen2/collectors/datafile/service/producer/DmaapProducerReactiveHttpClient.java
+++ datafile-dmaap-client/src/main/java/org/onap/dcaegen2/collectors/datafile/service/producer/DmaapProducerReactiveHttpClient.java
@@ -23,26 +23,29 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.net.URI;
 import java.nio.charset.StandardCharsets;
-import java.security.KeyManagementException;
-import java.security.KeyStoreException;
-import java.security.NoSuchAlgorithmException;
+import java.util.concurrent.Future;
+
+import javax.net.ssl.SSLContext;
 
 import org.apache.commons.codec.binary.Base64;
 import org.apache.commons.io.IOUtils;
+import org.apache.http.HttpResponse;
+import org.apache.http.client.methods.HttpPut;
+import org.apache.http.conn.ssl.NoopHostnameVerifier;
+import org.apache.http.entity.ByteArrayEntity;
+import org.apache.http.impl.nio.client.CloseableHttpAsyncClient;
+import org.apache.http.impl.nio.client.HttpAsyncClients;
+import org.apache.http.ssl.SSLContextBuilder;
 import org.onap.dcaegen2.collectors.datafile.config.DmaapPublisherConfiguration;
 import org.onap.dcaegen2.collectors.datafile.io.FileSystemResourceWrapper;
 import org.onap.dcaegen2.collectors.datafile.io.IFileSystemResource;
 import org.onap.dcaegen2.collectors.datafile.model.CommonFunctions;
 import org.onap.dcaegen2.collectors.datafile.model.ConsumerDmaapModel;
-import org.onap.dcaegen2.collectors.datafile.web.IRestTemplate;
-import org.onap.dcaegen2.collectors.datafile.web.RestTemplateWrapper;
+import org.onap.dcaegen2.collectors.datafile.service.HttpUtils;
+import org.onap.dcaegen2.collectors.datafile.web.PublishRedirectStrategy;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpHeaders;
-import org.springframework.http.HttpMethod;
-import org.springframework.http.MediaType;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.util.DefaultUriBuilderFactory;
 
 import reactor.core.publisher.Flux;
@@ -70,7 +73,7 @@ public class DmaapProducerReactiveHttpClient {
     private final String pwd;
 
     private IFileSystemResource fileResource;
-    private IRestTemplate restTemplate;
+    private CloseableHttpAsyncClient webClient;
 
     /**
      * Constructor DmaapProducerReactiveHttpClient.
@@ -78,7 +81,6 @@ public class DmaapProducerReactiveHttpClient {
      * @param dmaapPublisherConfiguration - DMaaP producer configuration object
      */
     public DmaapProducerReactiveHttpClient(DmaapPublisherConfiguration dmaapPublisherConfiguration) {
-
         this.dmaapHostName = dmaapPublisherConfiguration.dmaapHostName();
         this.dmaapPortNumber = dmaapPublisherConfiguration.dmaapPortNumber();
         this.dmaapTopicName = dmaapPublisherConfiguration.dmaapTopicName();
@@ -97,54 +99,70 @@ public class DmaapProducerReactiveHttpClient {
     public Flux<String> getDmaapProducerResponse(ConsumerDmaapModel consumerDmaapModel) {
         logger.trace("Entering getDmaapProducerResponse with {}", consumerDmaapModel);
         try {
-            HttpHeaders headers = new HttpHeaders();
-            headers.setContentType(MediaType.parseMediaType(dmaapContentType));
-            addMetaDataToHead(consumerDmaapModel, headers);
-
-            addUserCredentialsToHead(headers);
-
-            IFileSystemResource fileSystemResource = getFileSystemResource();
-            fileSystemResource.setPath(consumerDmaapModel.getLocation());
-            InputStream fileInputStream = fileSystemResource.getInputStream();
-            HttpEntity<byte[]> request = addFileToRequest(fileInputStream, headers);
-
-
             logger.trace("Starting to publish to DR");
-            ResponseEntity<String> responseEntity = getRestTemplate().exchange(getUri(consumerDmaapModel.getName()),
-                    HttpMethod.PUT, request, String.class);
 
-            return Flux.just(responseEntity.getStatusCode().toString());
+            webClient = getWebClient();
+            webClient.start();
+
+            HttpPut put = new HttpPut();
+            prepareHead(consumerDmaapModel, put);
+            prepareBody(consumerDmaapModel, put);
+            addUserCredentialsToHead(put);
+
+            Future<HttpResponse> future = webClient.execute(put, null);
+            HttpResponse response = future.get();
+            logger.trace(response.toString());
+            webClient.close();
+            handleHttpResponse(response);
+            return Flux.just(response.toString());
         } catch (Exception e) {
             logger.error("Unable to send file to DataRouter. Data: {}", consumerDmaapModel, e);
             return Flux.empty();
         }
     }
 
-    private void addUserCredentialsToHead(HttpHeaders headers) {
+    private void handleHttpResponse(HttpResponse response) {
+        int statusCode = response.getStatusLine().getStatusCode();
+        if (HttpUtils.isSuccessfulResponseCode(statusCode)) {
+            logger.trace("Publish to DR successful!");
+        } else {
+            logger.error("Publish to DR unsuccessful, response code: " + statusCode);
+        }
+    }
+
+    private void addUserCredentialsToHead(HttpPut put) {
         String plainCreds = user + ":" + pwd;
         byte[] plainCredsBytes = plainCreds.getBytes(StandardCharsets.ISO_8859_1);
         byte[] base64CredsBytes = Base64.encodeBase64(plainCredsBytes);
         String base64Creds = new String(base64CredsBytes);
         logger.trace("base64Creds...: {}", base64Creds);
-        headers.add("Authorization", "Basic " + base64Creds);
+        put.addHeader("Authorization", "Basic " + base64Creds);
     }
 
-    private void addMetaDataToHead(ConsumerDmaapModel consumerDmaapModel, HttpHeaders headers) {
-        JsonElement metaData = new JsonParser().parse(CommonFunctions.createJsonBody(consumerDmaapModel));
-        metaData.getAsJsonObject().remove(NAME_JSON_TAG).getAsString();
+    private void prepareHead(ConsumerDmaapModel model, HttpPut put) {
+        put.addHeader(HttpHeaders.CONTENT_TYPE, dmaapContentType);
+        JsonElement metaData = new JsonParser().parse(CommonFunctions.createJsonBody(model));
+        String name = metaData.getAsJsonObject().remove(NAME_JSON_TAG).getAsString();
         metaData.getAsJsonObject().remove(LOCATION_JSON_TAG);
-        headers.set(X_ATT_DR_META, metaData.toString());
-    }
-    private HttpEntity<byte[]> addFileToRequest(InputStream inputStream, HttpHeaders headers)
-            throws IOException {
-        return new HttpEntity<>(IOUtils.toByteArray(inputStream), headers);
+        put.addHeader(X_ATT_DR_META, metaData.toString());
+        put.setURI(getUri(name));
     }
 
-    private IRestTemplate getRestTemplate() throws NoSuchAlgorithmException, KeyManagementException, KeyStoreException {
-        if (restTemplate == null) {
-            restTemplate = new RestTemplateWrapper();
+    private void prepareBody(ConsumerDmaapModel model, HttpPut put) {
+        String fileLocation = model.getLocation();
+        IFileSystemResource fileSystemResource = getFileSystemResource();
+        fileSystemResource.setPath(fileLocation);
+        InputStream fileInputStream = null;
+        try {
+            fileInputStream = fileSystemResource.getInputStream();
+        } catch (IOException e) {
+            logger.error("Unable to get stream from filesystem.", e);
         }
-        return restTemplate;
+        try {
+            put.setEntity(new ByteArrayEntity(IOUtils.toByteArray(fileInputStream)));
+        } catch (IOException e) {
+            logger.error("Unable to set put request body from ByteArray.", e);
+        }
     }
 
     private URI getUri(String fileName) {
@@ -164,7 +182,26 @@ public class DmaapProducerReactiveHttpClient {
         fileResource = fileSystemResource;
     }
 
-    protected void setRestTemplate(IRestTemplate restTemplate) {
-        this.restTemplate = restTemplate;
+    protected CloseableHttpAsyncClient getWebClient() {
+        if (webClient != null) {
+            return webClient;
+        }
+        SSLContext sslContext = null;
+        try {
+            sslContext = new SSLContextBuilder().loadTrustMaterial(null, (certificate, authType) -> true).build();
+        } catch (Exception e) {
+            logger.trace("Unable to get sslContext.", e);
+        }
+        //@formatter:off
+        return HttpAsyncClients.custom()
+                .setSSLContext(sslContext)
+                .setSSLHostnameVerifier(new NoopHostnameVerifier())
+                .setRedirectStrategy(PublishRedirectStrategy.INSTANCE)
+                .build();
+        //@formatter:on
+    }
+
+    protected void setWebClient(CloseableHttpAsyncClient client) {
+        this.webClient = client;
     }
 }
