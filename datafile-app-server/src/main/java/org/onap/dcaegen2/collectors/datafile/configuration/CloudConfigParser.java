985747a56b32c3236bd61120916d8bc1d77cea61
Jan Malkiewicz <jan.malkiewicz@nokia.com>
2020-07-15 15:28:41 +0200

    Add sftp strict host key checking to DFC in 5G-bulkpm
    Issue-ID: DCAEGEN2-2219
    
    Signed-off-by: Jan Malkiewicz <jan.malkiewicz@nokia.com>
    Change-Id: Iadf6c6bd743c42ebb3bf9ad8ac443fc0f3f58063

--- datafile-app-server/src/main/java/org/onap/dcaegen2/collectors/datafile/configuration/CloudConfigParser.java
+++ datafile-app-server/src/main/java/org/onap/dcaegen2/collectors/datafile/configuration/CloudConfigParser.java
@@ -20,15 +20,13 @@ package org.onap.dcaegen2.collectors.datafile.configuration;
 
 import com.google.gson.JsonElement;
 import com.google.gson.JsonObject;
-
 import java.util.HashMap;
 import java.util.Iterator;
 import java.util.Map;
 import java.util.Map.Entry;
+import java.util.Properties;
 import java.util.Set;
-
 import javax.validation.constraints.NotNull;
-
 import org.onap.dcaegen2.collectors.datafile.exceptions.DatafileTaskException;
 
 /**
@@ -38,6 +36,7 @@ import org.onap.dcaegen2.collectors.datafile.exceptions.DatafileTaskException;
  * @author <a href="mailto:henrik.b.andersson@est.tech">Henrik Andersson</a>
  */
 public class CloudConfigParser {
+
     private static final String DMAAP_SECURITY_TRUST_STORE_PATH = "dmaap.security.trustStorePath";
     private static final String DMAAP_SECURITY_TRUST_STORE_PASS_PATH = "dmaap.security.trustStorePasswordPath";
     private static final String DMAAP_SECURITY_KEY_STORE_PATH = "dmaap.security.keyStorePath";
@@ -45,22 +44,25 @@ public class CloudConfigParser {
     private static final String DMAAP_SECURITY_ENABLE_DMAAP_CERT_AUTH = "dmaap.security.enableDmaapCertAuth";
     private static final String CONFIG = "config";
 
+    public static final String KNOWN_HOSTS_FILE_PATH = "KNOWN_HOSTS_FILE_PATH";
+
+    private final Properties systemEnvironment;
+
     private final JsonObject jsonObject;
 
-    public CloudConfigParser(JsonObject jsonObject) {
+    public CloudConfigParser(JsonObject jsonObject, Properties properties) {
         this.jsonObject = jsonObject.getAsJsonObject(CONFIG);
-
+        this.systemEnvironment = properties;
     }
 
     /**
      * Get the publisher configurations.
      *
-     * @return a map with change identifier as key and the connected publisher configuration as
-     *         value.
-     *
+     * @return a map with change identifier as key and the connected publisher configuration as value.
      * @throws DatafileTaskException if a member of the configuration is missing.
      */
-    public @NotNull Map<String, PublisherConfiguration> getDmaapPublisherConfigurations() throws DatafileTaskException {
+    public @NotNull Map<String, PublisherConfiguration> getDmaapPublisherConfigurations()
+        throws DatafileTaskException {
         JsonObject producerCfgs = jsonObject.get("streams_publishes").getAsJsonObject();
         Iterator<String> changeIdentifierList = producerCfgs.keySet().iterator();
         Map<String, PublisherConfiguration> result = new HashMap<>();
@@ -98,7 +100,8 @@ public class CloudConfigParser {
         JsonObject consumerCfg = jsonObject.get("streams_subscribes").getAsJsonObject();
         Set<Entry<String, JsonElement>> topics = consumerCfg.entrySet();
         if (topics.size() != 1) {
-            throw new DatafileTaskException("Invalid configuration, number of topic must be one, config: " + topics);
+            throw new DatafileTaskException(
+                "Invalid configuration, number of topic must be one, config: " + topics);
         }
         JsonObject topic = topics.iterator().next().getValue().getAsJsonObject();
         JsonObject dmaapInfo = get(topic, "dmaap_info").getAsJsonObject();
@@ -114,6 +117,28 @@ public class CloudConfigParser {
     }
 
     /**
+     * Get the sFTP configuration.
+     *
+     * @return the sFTP configuration.
+     * @throws DatafileTaskException if a member of the configuration is missing.
+     */
+    public @NotNull SfptConfig getSftpConfig() throws DatafileTaskException {
+        String filePath = determineKnownHostsFilePath();
+        return new ImmutableSfptConfig.Builder() //
+            .strictHostKeyChecking(getAsBoolean(jsonObject, "sftp.security.strictHostKeyChecking"))
+            .knownHostsFilePath(filePath)
+            .build();
+    }
+
+    private String determineKnownHostsFilePath() {
+        String filePath = "";
+        if (systemEnvironment != null && systemEnvironment.getProperty(KNOWN_HOSTS_FILE_PATH) != null) {
+            filePath = systemEnvironment.getProperty(KNOWN_HOSTS_FILE_PATH);
+        }
+        return filePath;
+    }
+
+    /**
      * Get the security configuration for communication with the xNF.
      *
      * @return the xNF communication security configuration.
@@ -124,11 +149,13 @@ public class CloudConfigParser {
             .keyCert(getAsString(jsonObject, "dmaap.ftpesConfig.keyCert"))
             .keyPasswordPath(getAsString(jsonObject, "dmaap.ftpesConfig.keyPasswordPath"))
             .trustedCa(getAsString(jsonObject, "dmaap.ftpesConfig.trustedCa"))
-            .trustedCaPasswordPath(getAsString(jsonObject, "dmaap.ftpesConfig.trustedCaPasswordPath")) //
+            .trustedCaPasswordPath(
+                getAsString(jsonObject, "dmaap.ftpesConfig.trustedCaPasswordPath")) //
             .build();
     }
 
-    private static @NotNull JsonElement get(JsonObject obj, String memberName) throws DatafileTaskException {
+    private static @NotNull JsonElement get(JsonObject obj, String memberName)
+        throws DatafileTaskException {
         JsonElement elem = obj.get(memberName);
         if (elem == null) {
             throw new DatafileTaskException("Could not find member: " + memberName + " in: " + obj);
@@ -136,11 +163,18 @@ public class CloudConfigParser {
         return elem;
     }
 
-    private static @NotNull String getAsString(JsonObject obj, String memberName) throws DatafileTaskException {
+    private static @NotNull String getAsString(JsonObject obj, String memberName)
+        throws DatafileTaskException {
         return get(obj, memberName).getAsString();
     }
 
-    private static @NotNull JsonObject getAsJson(JsonObject obj, String memberName) throws DatafileTaskException {
+    private static @NotNull Boolean getAsBoolean(JsonObject obj, String memberName)
+        throws DatafileTaskException {
+        return get(obj, memberName).getAsBoolean();
+    }
+
+    private static @NotNull JsonObject getAsJson(JsonObject obj, String memberName)
+        throws DatafileTaskException {
         return get(obj, memberName).getAsJsonObject();
     }
 
---
+++ datafile-app-server/src/main/java/org/onap/dcaegen2/collectors/datafile/configuration/SfptConfig.java
@@ -0,0 +1,41 @@
+/*-
+ * ============LICENSE_START=======================================================
+ * Copyright (C) 2020 NOKIA Intellectual Property. All rights reserved.
+ * ================================================================================
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ * ============LICENSE_END=========================================================
+ */
+
+package org.onap.dcaegen2.collectors.datafile.configuration;
+
+import java.io.Serializable;
+import org.immutables.gson.Gson;
+import org.immutables.value.Value;
+import org.springframework.stereotype.Component;
+
+@Component
+@Value.Style(builder = "new")
+@Value.Immutable
+@Gson.TypeAdapters
+public abstract class SfptConfig implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+
+    @Value.Parameter
+    public abstract Boolean strictHostKeyChecking();
+
+    @Value.Parameter
+    public abstract String knownHostsFilePath();
+}
