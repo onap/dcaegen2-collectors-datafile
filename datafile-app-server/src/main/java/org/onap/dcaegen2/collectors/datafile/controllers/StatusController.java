8cc9e64a23ab773440a1f51d604a09c78f160283
ajay_dp001 <ajay.deep.singh@est.tech>
2021-01-29 17:44:27 +0530

    [DCAE-dfc] Vulnerability Removal for Datafile Collector
        - swagger version 2.9.2 to 3.0.0
        - commons-io version 1.3.2 to 2.8.0
        - tomcat-embed-core version 9.0.36 to 9.0.39
        - pom.xml refactoring
    
    Issue-ID: DCAEGEN2-2591
    Signed-off-by: ajay_dp001 <ajay.deep.singh@est.tech>
    Change-Id: I40e65a6936fc118299cd575df34c51730a25f424

--- datafile-app-server/src/main/java/org/onap/dcaegen2/collectors/datafile/controllers/StatusController.java
+++ datafile-app-server/src/main/java/org/onap/dcaegen2/collectors/datafile/controllers/StatusController.java
@@ -1,6 +1,7 @@
 /*-
  * ============LICENSE_START======================================================================
- * Copyright (C) 2018 NOKIA Intellectual Property, 2018 Nordix Foundation. All rights reserved.
+ * Copyright (C) 2018 NOKIA Intellectual Property. All rights reserved.
+ * Copyright (C) 2018-2021 Nordix Foundation. All rights reserved.
  * ===============================================================================================
  * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except
  * in compliance with the License. You may obtain a copy of the License at
@@ -19,11 +20,10 @@ package org.onap.dcaegen2.collectors.datafile.controllers;
 import static org.onap.dcaegen2.collectors.datafile.model.logging.MappedDiagnosticContext.ENTRY;
 import static org.onap.dcaegen2.collectors.datafile.model.logging.MappedDiagnosticContext.EXIT;
 
-import io.swagger.annotations.Api;
-import io.swagger.annotations.ApiOperation;
 import io.swagger.annotations.ApiResponse;
 import io.swagger.annotations.ApiResponses;
-
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.onap.dcaegen2.collectors.datafile.model.Counters;
 import org.onap.dcaegen2.collectors.datafile.model.logging.MappedDiagnosticContext;
 import org.onap.dcaegen2.collectors.datafile.tasks.ScheduledTasks;
@@ -42,7 +42,7 @@ import reactor.core.publisher.Mono;
  * REST Controller to check the heart beat and status of the DFC.
  */
 @RestController
-@Api(value = "StatusController")
+@Tag(name = "StatusController")
 public class StatusController {
 
     private static final Logger logger = LoggerFactory.getLogger(StatusController.class);
@@ -60,7 +60,7 @@ public class StatusController {
      * @return the heart beat status of DFC.
      */
     @GetMapping("/heartbeat")
-    @ApiOperation(value = "Returns liveness of DATAFILE service")
+    @Operation(summary = "Returns liveness of DATAFILE service")
     @ApiResponses(
         value = { //
             @ApiResponse(code = 200, message = "DATAFILE service is living"),
@@ -85,7 +85,7 @@ public class StatusController {
      * @return information.
      */
     @GetMapping("/status")
-    @ApiOperation(value = "Returns status and statistics of DATAFILE service")
+    @Operation(summary = "Returns status and statistics of DATAFILE service")
     @ApiResponses(
         value = { //
             @ApiResponse(code = 200, message = "DATAFILE service is living"),
@@ -101,5 +101,4 @@ public class StatusController {
         logger.info(EXIT, "Status request");
         return response;
     }
-
 }
