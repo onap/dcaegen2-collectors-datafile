8dc20b360d0c9613c70c181370c41b2280c4413b
Tomasz Wrobel <tomasz.wrobel@nokia.com>
2022-05-13 09:01:26 +0200

    Make TLS connection optional
    Issue-ID: DCAEGEN2-3039
    Signed-off-by: Tomasz Wrobel <tomasz.wrobel@nokia.com>
    Change-Id: Id637ba17c655407009a4f40f6c93f518b99e45ff

--- Changelog.md
+++ Changelog.md
@@ -4,6 +4,10 @@ All notable changes to this project will be documented in this file.
 The format is based on [Keep a Changelog](http://keepachangelog.com/)
 and this project adheres to [Semantic Versioning](http://semver.org/).
 
+## [1.8.0] - 2022/03/10
+### Changed
+- [DCAEGEN2-3039] Make certificate dependencies configurable
+
 ## [1.7.1] - 2022/03/10
 ### Changed
 - [DCAEGEN2-3104] Fix application startup
--- datafile-app-server/src/main/java/org/onap/dcaegen2/collectors/datafile/configuration/AppConfig.java
+++ datafile-app-server/src/main/java/org/onap/dcaegen2/collectors/datafile/configuration/AppConfig.java
@@ -234,6 +234,10 @@ public class AppConfig {
         this.certificateConfiguration = certificateConfig;
         this.sftpConfiguration = sftpConfig;
 
+        if (!certificateConfig.enableCertAuth()) {
+            logger.debug("External TLS certificate disabled, skipping setup HTTPS client");
+            return;
+        }
         HttpsClientConnectionManagerUtil.setupOrUpdate(certificateConfig.keyCert(), certificateConfig.keyPasswordPath(),
             certificateConfig.trustedCa(), certificateConfig.trustedCaPasswordPath(),
             certificateConfig.httpsHostnameVerify());
